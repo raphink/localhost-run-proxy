38a4f9cba284af09c0031d3790077279a1e69bc9
Raphaël Pinson <raphink@gmail.com>
2022-04-06 14:17:37 +0200

    Use an ifram

--- page-build/docker-entrypoint.sh
+++ page-build/docker-entrypoint.sh
@@ -6,7 +6,7 @@ while [ 1 ]; do
 	cat <<EOF > /var/proxy/index.html
 <html>
 <body>
-Redirecting to <a href="$URL" target="_blank">Hubble UI</a>.
+<iframe src="$URL" style="width:100vw;height:100vh;"></iframe>
 </body>
 </html>
 EOF
